27893af7f1af825f7ba7f980ad10aef88af32b18
lunalo57 <lunalo.dev@gmail.com>
2022-11-03 11:10:31 +0100

    finalisation

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+
+const navbar = document.getElementById('navbar');
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY < lastScroll){
+        navbar.style.top = 0
+    }else{
+        navbar.style.top = "-60px"
+    }
+    lastScroll = window.scrollY;
+})
